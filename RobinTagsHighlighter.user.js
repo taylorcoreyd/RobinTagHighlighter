4ee6a9ccc93823019a599d6dcea40cc9bcda3892
Corey Taylor <taylorcoreyd@icloud.com>
2016-04-02 06:07:55 -0500

    Comments and Cleanup

--- RobinTagsHighlighter.user.js
+++ RobinTagsHighlighter.user.js
@@ -10,39 +10,68 @@
 
 
 'use strict';
+
+
+// The list of all messages.
 var messageList = $("#robinChatMessageList");
+
 messageList.bind("DOMNodeInserted", function() {
+    
+    var lastMessage = $("#robinChatMessageList .robin-message").last();
+    
+    // First let's highlight messages with tags
+    
+    // Regex of {anything}[{char}]{anything}
     var tag = /.*\[.\].*/
-    var spam = /\[Robin Autovoter.*\]/
-    var x = $("#robinChatMessageList .robin-message").last();
-    var message = x.find(".robin-message--message");
-    if (message.text().search(spam) != -1) {
-        x.remove();
-    }
-    if (message.text().search(tag) != -1) {
-        message.css("background-color", "#b3f0ff");
-    }
-    if (message.text().search("[spam] I'm using Robin Tag Highlighter to highlight and type tags: http://bit.ly/1oqH7WR") != -1) {
-        x.remove();
+    var messageText = lastMessage.find(".robin-message--message");
+    if (messageText.text().search(tag) != -1) {
+        messageText.css("background-color", "#b3f0ff");
     }
     
-    if (numberUsed >= 100) {
-        inputBox.val("[spam] I'm using Robin Tag Highlighter to highlight and type tags: http://bit.ly/1oqH7WR").submit();
-        numberUsed = 0;
+    // Deal with some spam. Remove it.
+    
+    // Regex of all known spam
+    // Generally in form of [{script name}{gibberish}]|
+    var spam = new RegExp('' // When modifying this, be sure to add a | to the last regex
+                          + /\[Robin Autovoter.*\]|/.source
+                          + /\[spam.*\]|/.source
+                          + /\[Robin-Grow.*\]/.source
+                         );
+    if (messageText.text().search(spam) != -1) {
+        lastMessage.remove();
     }
+    
+    // Do a little spamming of our own. Feel free to delete the marked lines if you want to remove spam // mark
+    // It shouldn't spam aggressively, only once every 100 messages of which you tagged.
+    if (numberUsed >= 100) {    // mark
+        inputBox.val("[spam] I'm using Robin Tag Highlighter to highlight and type tags: http://bit.ly/1oqH7WR").submit(); // mark
+        numberUsed = 0;          // mark            ^ this one is marked too
+    }                            // mark
 });
+var numberUsed = 0; // For keeping track of spamming purposes. Will be reset occasionally.
 
-var numberUsed = 0;
+// The input element in which user types
 var inputBox = $(document).find(".c-form-control.text-counter-input");
+// on input = whenever user types in or makes some change.
 inputBox.on("input", function() {
+    // Looking for ,{char}
     var re = /\,[A-z]/;
+    
+    // Get the value from the inputBox
     var inVal = $( this ).val();
+    // Check it for ,{char}, mark index of it
     var i = inVal.search(re);
+    // If ,{char} is found
     if (i != -1) {
+        // increment to account for position of comma
         i = i + 1;
+        // Increment number used for spamming purposes
         numberUsed = numberUsed + 1;
+        // replaces text with tagged version
         inVal = inVal.replace(re, "[" + inVal.charAt(i).toUpperCase() + "]");
+        // Reset the i back so we don't keep incrementing.
         i = -1;
     }
+    // put the tagged version into the input box.
     $( this ).val(inVal);
 });
