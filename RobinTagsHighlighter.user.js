3effc18b739fb6f56d7de6f5ba1a744946df1cc1
Corey Taylor <taylorcoreyd@icloud.com>
2016-04-02 05:06:45 -0500

    Added spam features

--- RobinTagsHighlighter.user.js
+++ RobinTagsHighlighter.user.js
@@ -3,30 +3,46 @@
 // @namespace    https://github.com/taylorcoreyd
 // @version      0.0.1
 // @description  Highlights Tags in Robin chats!
-// @author       Chr12t0pher and cdtdev
+// @author       cdtdev
 // @include      https://www.reddit.com/robin*
 // @grant        none
 // ==/UserScript==
 
-// Derived from the original Robin Username Highlighter
-// Because I don't know Javascript ;)
-
 
 'use strict';
-var tag = /.*\[.\].*/
-$("#robinChatMessageList").bind("DOMNodeInserted", function() {
+var messageList = $("#robinChatMessageList");
+messageList.bind("DOMNodeInserted", function() {
+    var tag = /.*\[.\].*/
+    var spam = /\[Robin Autovoter.*\]/
     var x = $("#robinChatMessageList .robin-message").last();
     var message = x.find(".robin-message--message");
+    if (message.text().search(spam) != -1) {
+        x.remove();
+    }
     if (message.text().search(tag) != -1) {
         message.css("background-color", "#b3f0ff");
     }
+    if (message.text().search("[spam] I'm using Robin Tag Highlighter to highlight and type tags: http://bit.ly/1oqH7WR") != -1) {
+        x.remove();
+    }
+    
+    if (numberUsed >= 100) {
+        inputBox.val("[spam] I'm using Robin Tag Highlighter to highlight and type tags: http://bit.ly/1oqH7WR").submit();
+        numberUsed = 0;
+    }
 });
 
+var numberUsed = 0;
 var inputBox = $(document).find(".c-form-control.text-counter-input");
 inputBox.on("input", function() {
     var re = /\,[A-z]/;
     var inVal = $( this ).val();
-    var i = inVal.search(re) + 1;
-    inVal = inVal.replace(re, "[" + inVal.charAt(i).toUpperCase() + "]");
+    var i = inVal.search(re);
+    if (i != -1) {
+        i = i + 1;
+        numberUsed = numberUsed + 1;
+        inVal = inVal.replace(re, "[" + inVal.charAt(i).toUpperCase() + "]");
+        i = -1;
+    }
     $( this ).val(inVal);
 });
